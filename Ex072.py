7de23f99f9da8c1342c450c1b1db806db0df39e7
RAFAEL LUIZ SIQUEIRA <Rafael.l.siqueira@hotmail.com>
2022-02-07 22:06:41 -0300

    Exercicios Mundo 3

---
+++ Ex072.py
@@ -0,0 +1,13 @@
+contador = ('zero', 'um', 'dois', 'três', 'quatro', 'cinco', 'seis', 'sete', 'oito', 'nove', 'dez',
+            'onde', 'dose', 'treze', 'quatorze', 'quinze', 'dezesseis', 'dezessete', 'dezoito', 'dezenove', "vinte")
+
+while True:
+    num = int(input('Digite um número entre 0 e 20: '))
+    if 0 <= num <= 20:
+        print(f'Você digitou o número {contador[num]}')
+    elif num > 20:
+        break
+    else:
+        print('Números negativos invalidos, Tente Novamente. ')
+print('-='*20)
+print('Programa finalizado com sucesso!!!')
